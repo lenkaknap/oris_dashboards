5834282fd5fcdd9a5d90d1cc2c8ed876d423c7c8
lenkaknap <knapova.ll@gmail.com>
2017-12-11 17:41:32 +0100

    Add logos and text to web page

--- application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template('my_template.html', map=url_for('static', filename='map.html'), stacked=url_for('static', filename='stacked-bar.html'), stacked_km=url_for('static', filename='km_months_stacked.html'), controls=url_for('static', filename='controls_months_stacked.html'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
